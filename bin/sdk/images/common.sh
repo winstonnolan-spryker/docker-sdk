3214209675e8b7dad4f9eb6fb9564f71380cdf5f
Aleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2022-01-14 15:05:33 +0200

    Update common.sh

--- bin/sdk/images/common.sh
+++ bin/sdk/images/common.sh
@@ -206,7 +206,7 @@ function Images::tagImages() {
         echo "${SPRYKER_PROJECT_NAME}-${application}:latest";
         docker tag "${SPRYKER_DOCKER_PREFIX}_app:${tag}" "${aws_account_id}.dkr.ecr.eu-central-1.amazonaws.com/${SPRYKER_PROJECT_NAME}-${application}:latest"
     done
-     spryker_app:1.0 
+    
     docker tag "${SPRYKER_DOCKER_PREFIX}_frontend:${tag}" "${aws_account_id}.dkr.ecr.eu-central-1.amazonaws.com/${SPRYKER_PROJECT_NAME}-frontend:latest"
     docker tag "${SPRYKER_DOCKER_PREFIX}_jenkins:${tag}" "${aws_account_id}.dkr.ecr.eu-central-1.amazonaws.com/${SPRYKER_PROJECT_NAME}-jenkins:latest"
     docker tag "${SPRYKER_DOCKER_PREFIX}_pipeline:${tag}" "${aws_account_id}.dkr.ecr.eu-central-1.amazonaws.com/${SPRYKER_PROJECT_NAME}-pipeline:latest"
