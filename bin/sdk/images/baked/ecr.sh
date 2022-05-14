8d145ef8a4e9abf7e74e0162ec755f6b39636862
Vadym Khanko <vadimhanko@yahoo.com>
2022-05-14 10:43:59 +0300

    Added debugging

--- bin/sdk/images/baked/ecr.sh
+++ bin/sdk/images/baked/ecr.sh
@@ -27,8 +27,9 @@ function Images::tagApplications() {
     # docker tag $${INIT_IMAGE_PREFIX}_app:1.0-yves  $${YVES_ECR_REPO}:$${DESIRED_IMAGE_TAG}
     # 262925510123.dkr.ecr.eu-west-1.amazonaws.com/khanko-staging-jenkins
 
-    echo "SPRYKER_DOCKER_PREFIX: ${SPRYKER_DOCKER_PREFIX} tag: ${tag}" >> vvkdebug.txt
-    echo "AWS_ACCOUNT_ID: ${AWS_ACCOUNT_ID} AWS_REGION: ${AWS_REGION} SPRYKER_PROJECT_NAME: ${SPRYKER_PROJECT_NAME}" >> vvkdebug.txt
+    # Debug calls
+    Console::error "SPRYKER_DOCKER_PREFIX: ${SPRYKER_DOCKER_PREFIX} tag: ${tag}"
+    Console::error "AWS_ACCOUNT_ID: ${AWS_ACCOUNT_ID} AWS_REGION: ${AWS_REGION} SPRYKER_PROJECT_NAME: ${SPRYKER_PROJECT_NAME}"
 
     docker tag "${SPRYKER_DOCKER_PREFIX}_jenkins:${tag}" "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${SPRYKER_PROJECT_NAME}-jenkins:latest"
     docker tag "${SPRYKER_DOCKER_PREFIX}_pipeline:${tag}" "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${SPRYKER_PROJECT_NAME}-pipeline:latest"
@@ -45,7 +46,8 @@ function Images::tagFrontend() {
 function Images::push() {
     Console::verbose "${INFO}Pushing images to AWS ECR${NC}"
 
-    echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    # Debug calls
+    Console::error "*******************PUSH FUNCTION**********************"
 
     docker images | grep ecr
     for application in "${SPRYKER_APPLICATIONS[@]}"; do
